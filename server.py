98f9969913bce3326373f3c159481ed1f2fa3985
SIVAPRAKASH5668 <144685762+SIVAPRAKASH5668@users.noreply.github.com>
2024-04-01 20:07:31 +0530

    loosu paruda!!

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
-
+import pyautogui as pi
 with socketserver.TCPServer(("",80),http.server.SimpleHTTPRequestHandler) as server:
     server.serve_forever()
+py.press(('t','h','i','l','l','a','i',' ','o','r','u',' ','l','o','o','s','u',' ','k','o','o','t','h','i'))
